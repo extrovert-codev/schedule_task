24e497b1443c5aa0531f00cbc1510878b75a30e5
Extrovert Code Developer <extrovertcodev@gmail.com>
2021-07-30 13:16:09 +0700

    coba local 3

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -30,8 +30,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "31822a45d6abb2160fd3a4d45ae5209a",
-"/": "31822a45d6abb2160fd3a4d45ae5209a",
+"index.html": "b88c9b123cc7b8d8a41cca206bcc9901",
+"/": "b88c9b123cc7b8d8a41cca206bcc9901",
 "main.dart.js": "104f478cb0ee92d7928b46b6577088d6",
 "manifest.json": "f9aad30ab76529de400ea3503e0d52df",
 "version.json": "4aea125d56c38562d5b2e7391bf06d44"
